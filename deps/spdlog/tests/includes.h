c46c84aeeb02db3ebfe4ed10e372ee9173658fd7
Sebastian Wallat <sebastian@wallat.io>
2019-04-26 16:51:52 +0200

    ReAdded spdlog file

---
+++ deps/spdlog/tests/includes.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <ostream>
+#include <chrono>
+#include <exception>
+#include "catch.hpp"
+#include "utils.h"
+
+#define SPDLOG_TRACE_ON
+#define SPDLOG_DEBUG_ON
+
+#include "../include/spdlog/spdlog.h"
+#include "../include/spdlog/sinks/null_sink.h"
+#include "../include/spdlog/sinks/ostream_sink.h"
+
