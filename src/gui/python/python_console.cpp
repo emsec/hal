9cdfcd785a1fd768ba2805cde0b9b2834da64fc1
Sebastian Maaßen <smaassen94@gmx.de>
2019-04-29 18:42:18 +0200

    Demo/light color theme v2 (#44)
    Add Sunny stylesheet

--- src/gui/python/python_console.cpp
+++ src/gui/python/python_console.cpp
@@ -9,6 +9,8 @@
 
 #include "python/python_console_history.h"
 
+#include "python/python_console_qss_adapter.h"
+
 python_console::python_console(QWidget* parent)
     : QTextEdit(parent), m_standard_prompt(">>> "), m_compound_prompt("... "), m_prompt_block_number(0), m_prompt_length(0), m_prompt_end_position(0), m_compound_prompt_end_position(0),
       m_in_compound_prompt(false), m_in_completion(false), m_current_compound_input(""), m_current_input(""), m_current_history_index(-1), m_current_completer_index(0),
@@ -18,9 +20,9 @@ python_console::python_console(QWidget* parent)
     setUndoRedoEnabled(false);
     ensureCursorVisible();
 
-    m_standard_color = QColor("#BBBBBB");
-    m_error_color    = QColor("#FF6B63");
-    m_prompt_color   = QColor("#917EB7");
+    m_standard_color = python_console_qss_adapter::instance()->standard_color();
+    m_error_color = python_console_qss_adapter::instance()->error_color();
+    m_prompt_color = python_console_qss_adapter::instance()->promt_color();
     g_python_context->set_console(this);
     g_python_context->interpret("print(\"Python \" + sys.version)", false);
     g_python_context->interpret("print(sys.executable + \" on \" + sys.platform)", false);
--- src/gui/selection_details_widget/table_selector_widget.cpp
+++ src/gui/selection_details_widget/table_selector_widget.cpp
@@ -25,7 +25,7 @@ table_selector_widget::table_selector_widget(std::set<std::shared_ptr<gate>> tab
                                             << "Name"
                                             << "Type");
     setEditTriggers(QAbstractItemView::NoEditTriggers);
-    setStyleSheet("QTableView {background-color: black; color: white; selection-background-color: grey;} QTableView::item {border-right : 1px solid rgb(169, 183, 198);};");
+    //setStyleSheet("QTableView {background-color: black; color: white; selection-background-color: grey;} QTableView::item {border-right : 1px solid rgb(169, 183, 198);};");
 
     int counter = 0;
     for (const auto& current_gate : table_data)
@@ -63,7 +63,7 @@ void table_selector_widget::keyPressEvent(QKeyEvent *event)
     if(event->key() == Qt::Key_Escape)
         close();
 
-    if(event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return)
+    if(event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return || event->key() == Qt::Key_Right)
     {
         QTableWidgetItem* id_item = this->item(selectedItems().first()->row(), 0);
         auto gate = g_netlist->get_gate_by_id(id_item->text().toInt());
