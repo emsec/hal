2310c4fc86bb4590cc81ad6decb64b213b637de2
Sebastian Wallat <sebastian@wallat.io>
2019-05-30 21:36:01 +0200

    Remove dirty flag from genversion.py

--- tools/genversion.py
+++ tools/genversion.py
@@ -19,8 +19,8 @@ def _execute_git_describe(cmd, regex_str):
 if __name__ == '__main__':
     # Use git describe to extract version information relative to the last tag
     commands = [
-        "git describe --tags --match \"v[0-9]*\" --broken --always --abbrev=9",
-        "git describe --tags --match \"v[0-9]*\" --always --abbrev=9"
+        "git describe --tags --match \"v[0-9]*\" --broken --dirty="" --always --abbrev=9",
+        "git describe --tags --match \"v[0-9]*\" --dirty="" --always --abbrev=9"
         ]
     regex_strings = [
         "v((?P<major>\d+)\.(?P<minor>\d+)\.?(?P<patch>\d+)?(\.?(?P<tweak>\d+))?)(-(?P<add_commit>\d+))?(-g(?P<hash>[0-9,a-f]{9}))?(-(?P<dirty>dirty))?(-(?P<broken>broken))?",
