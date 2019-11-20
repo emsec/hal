1d765589dfc92956f174a6cce0000630908ed112
Max Hoffmann <max.hoffmann@rub.de>
2019-11-20 15:54:15 +0100

    removed DEBUG define from def

--- include/def.h
+++ include/def.h
@@ -35,20 +35,14 @@
 #include <stdexcept>
 #include <stdlib.h>
 
-using i8 = int8_t;
-
+using i8  = int8_t;
 using i16 = int16_t;
-
 using i32 = int32_t;
-
 using i64 = int64_t;
 
-using u8 = uint8_t;
-
+using u8  = uint8_t;
 using u16 = uint16_t;
-
 using u32 = uint32_t;
-
 using u64 = uint64_t;
 
 namespace hal
@@ -64,8 +58,6 @@ namespace hal
 #endif
     using path = fs::path;
 }    // namespace hal
-
-#define DEBUG
 
 #define UNUSED(expr) (void)expr
 
