20cd4cc45b71cb666513b59fe8b8599be7320cd8
Sebastian Wallat <sebastian@wallat.io>
2019-12-17 13:12:36 +0100

    Added hal_add_plugin function

--- cmake/hal_cmake_tools.cmake
+++ cmake/hal_cmake_tools.cmake
@@ -208,152 +208,62 @@ endmacro(configure_files)
 #                     [NO_EXTRAS] [SYSTEM] [THIN_LTO] source1 [source2 ...])
 #
 function(hal_add_plugin target_name)
-    set(options  MODULE SHARED EXCLUDE_FROM_ALL NO_EXTRAS SYSTEM THIN_LTO)
-    set(oneValueArgs DESTINATION RENAME)
-    set(multiValueArgs TARGETS CONFIGURATIONS)
-    set(options MODULE SHARED EXCLUDE_FROM_ALL NO_EXTRAS SYSTEM THIN_LTO)
+    set(options  MODULE SHARED EXCLUDE_FROM_ALL INSTALL)
+    set(oneValueArgs RENAME)
+    set(multiValueArgs HEADER SOURCES LINK_LIBRARIES INCLUDES DEFINITIONS COMPILE_OPTIONS LINK_OPTIONS)
     cmake_parse_arguments(ARG "${options}" "${oneValueArgs}"
                           "${multiValueArgs}" ${ARGN} )
 
-    file(GLOB_RECURSE GRAPH_ALGORITHM_INC ${CMAKE_CURRENT_SOURCE_DIR}/include/*.h)
-    file(GLOB_RECURSE GRAPH_ALGORITHM_SRC ${CMAKE_CURRENT_SOURCE_DIR}/src/*.cpp)
-    file(GLOB_RECURSE PYTHON_BINDING_LIB_SRC ${CMAKE_CURRENT_SOURCE_DIR}/python/python_definitions.cpp)
 
-    add_library(${target_name} SHARED ${GRAPH_ALGORITHM_SRC} ${PYTHON_BINDING_LIB_SRC} ${GRAPH_ALGORITHM_INC})
-    target_include_directories(${TARGET_NAME}
+    if(ARG_MODULE AND ARG_SHARED)
+        message(FATAL_ERROR "Can't be both MODULE and SHARED")
+    elseif(ARG_SHARED)
+        set(lib_type SHARED)
+    else()
+        set(lib_type MODULE)
+    endif()
+
+    if(ARG_EXCLUDE_FROM_ALL)
+        set(exclude_from_all EXCLUDE_FROM_ALL)
+    endif()
+
+    add_library(${target_name} ${lib_type} ${exclude_from_all} ${ARG_HEADER} ${ARG_SOURCES} ${ARG_PY_SOURCES})
+
+    message("CMAKE_CURRENT_LIST_DIR is ${CMAKE_CURRENT_LIST_DIR}")
+
+    target_include_directories(${target_name}
                                PUBLIC
                                $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include>
                                $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}>
-                               $<BUILD_INTERFACE:${IGRAPH_INCLUDES}>
-                               $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+                               $<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/include>
                                $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
+                               ${ARG_INCLUDES}
                                )
-    target_compile_definitions(${TARGET_NAME} PUBLIC
-                               -DIGRAPH_VERSION_MAJOR_GUESS=${IGRAPH_VERSION_MAJOR_GUESS}
-                               -DIGRAPH_VERSION_MINOR_GUESS=${IGRAPH_VERSION_MINOR_GUESS}
-                               -DIGRAPH_VERSION_PATCH_GUESS=${IGRAPH_VERSION_PATCH_GUESS}
+    if (ARG_DEFINITIONS)
+    target_compile_definitions(${target_name}
+                               ${ARG_DEFINITIONS}
                                )
+    endif()
+
+    target_compile_options(${target_name}
+                           PUBLIC ${COMPILE_OPTIONS_PUBLIC}
+                           PRIVATE ${COMPILE_OPTIONS_PRIVATE}
+                           INTERFACE ${COMPILE_OPTIONS_INTERFACE}
+                           ${ARG_COMPILE_OPTIONS})
+    if (ARG_LINK_OPTIONS)
+        target_link_options(${target_name} ${ARG_LINK_OPTIONS})
+    endif()
     #Set shared library suffix for MacOS
     if(APPLE AND CMAKE_HOST_APPLE)
-        set_target_properties(${TARGET_NAME} PROPERTIES SUFFIX ".so")
-        if(APPLE AND CMAKE_HOST_APPLE)
-            set_target_properties(${TARGET_NAME} PROPERTIES
-                                  INSTALL_NAME_DIR ${PLUGIN_LIBRARY_INSTALL_DIRECTORY}
-                                  )
-        endif()
+        set_target_properties(${target_name} PROPERTIES SUFFIX ".so")
+        set_target_properties(${target_name} PROPERTIES INSTALL_NAME_DIR ${PLUGIN_LIBRARY_INSTALL_DIRECTORY})
     endif(APPLE AND CMAKE_HOST_APPLE)
-    target_link_libraries(${TARGET_NAME} hal::core hal::netlist ${PYTHON_LIBRARIES} pybind11::pybind11 ${IGRAPH_LIBRARIES})
-    install(TARGETS ${TARGET_NAME} LIBRARY DESTINATION ${PLUGIN_LIBRARY_INSTALL_DIRECTORY} PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE INCLUDES DESTINATION ${PLUGIN_INCLUDE_INSTALL_DIRECTORY})
-    install(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/include/ DESTINATION ${PLUGIN_INCLUDE_INSTALL_DIRECTORY}/graph_algorithm/include/)
+
+    target_link_libraries(${target_name}
+                          PUBLIC hal::core hal::netlist ${PYTHON_LIBRARIES} pybind11::pybind11
+                          ${ARG_LINK_LIBRARIES})
+    install(TARGETS ${target_name} LIBRARY DESTINATION ${PLUGIN_LIBRARY_INSTALL_DIRECTORY} PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE INCLUDES DESTINATION ${PLUGIN_INCLUDE_INSTALL_DIRECTORY})
     if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
-        add_sanitizers(${TARGET_NAME})
+        add_sanitizers(${target_name})
     endif()
-#    if(ARG_MODULE AND ARG_SHARED)
-#        message(FATAL_ERROR "Can't be both MODULE and SHARED")
-#    elseif(ARG_SHARED)
-#        set(lib_type SHARED)
-#    else()
-#        set(lib_type MODULE)
-#    endif()
-#
-#    if(ARG_EXCLUDE_FROM_ALL)
-#        set(exclude_from_all EXCLUDE_FROM_ALL)
-#    endif()
-#
-#    add_library(${target_name} ${lib_type} ${exclude_from_all} ${ARG_UNPARSED_ARGUMENTS})
-#
-#    if(ARG_SYSTEM)
-#        set(inc_isystem SYSTEM)
-#    endif()
-#
-#    target_include_directories(${target_name} ${inc_isystem}
-#                               PRIVATE ${PYBIND11_INCLUDE_DIR}  # from project CMakeLists.txt
-#                               PRIVATE ${pybind11_INCLUDE_DIR}  # from pybind11Config
-#                               PRIVATE ${PYTHON_INCLUDE_DIRS})
-#
-#    # Python debug libraries expose slightly different objects
-#    # https://docs.python.org/3.6/c-api/intro.html#debugging-builds
-#    # https://stackoverflow.com/questions/39161202/how-to-work-around-missing-pymodule-create2-in-amd64-win-python35-d-lib
-#    if(PYTHON_IS_DEBUG)
-#        target_compile_definitions(${target_name} PRIVATE Py_DEBUG)
-#    endif()
-#
-#    # The prefix and extension are provided by FindPythonLibsNew.cmake
-#    set_target_properties(${target_name} PROPERTIES PREFIX "${PYTHON_MODULE_PREFIX}")
-#    set_target_properties(${target_name} PROPERTIES SUFFIX "${PYTHON_MODULE_EXTENSION}")
-#
-#    # -fvisibility=hidden is required to allow multiple modules compiled against
-#    # different pybind versions to work properly, and for some features (e.g.
-#    # py::module_local).  We force it on everything inside the `pybind11`
-#    # namespace; also turning it on for a pybind module compilation here avoids
-#    # potential warnings or issues from having mixed hidden/non-hidden types.
-#    set_target_properties(${target_name} PROPERTIES CXX_VISIBILITY_PRESET "hidden")
-#    set_target_properties(${target_name} PROPERTIES CUDA_VISIBILITY_PRESET "hidden")
-#
-#    if(WIN32 OR CYGWIN)
-#        # Link against the Python shared library on Windows
-#        target_link_libraries(${target_name} PRIVATE ${PYTHON_LIBRARIES})
-#    elseif(APPLE)
-#        # It's quite common to have multiple copies of the same Python version
-#        # installed on one's system. E.g.: one copy from the OS and another copy
-#        # that's statically linked into an application like Blender or Maya.
-#        # If we link our plugin library against the OS Python here and import it
-#        # into Blender or Maya later on, this will cause segfaults when multiple
-#        # conflicting Python instances are active at the same time (even when they
-#        # are of the same version).
-#
-#        # Windows is not affected by this issue since it handles DLL imports
-#        # differently. The solution for Linux and Mac OS is simple: we just don't
-#        # link against the Python library. The resulting shared library will have
-#        # missing symbols, but that's perfectly fine -- they will be resolved at
-#        # import time.
-#
-#        target_link_libraries(${target_name} PRIVATE "-undefined dynamic_lookup")
-#
-#        if(ARG_SHARED)
-#            # Suppress CMake >= 3.0 warning for shared libraries
-#            set_target_properties(${target_name} PROPERTIES MACOSX_RPATH ON)
-#        endif()
-#    endif()
-#
-#    # Make sure C++11/14 are enabled
-#    if(CMAKE_VERSION VERSION_LESS 3.3)
-#        target_compile_options(${target_name} PUBLIC ${PYBIND11_CPP_STANDARD})
-#    else()
-#        target_compile_options(${target_name} PUBLIC $<$<COMPILE_LANGUAGE:CXX>:${PYBIND11_CPP_STANDARD}>)
-#    endif()
-#
-#    if(ARG_NO_EXTRAS)
-#        return()
-#    endif()
-#
-#    _pybind11_add_lto_flags(${target_name} ${ARG_THIN_LTO})
-#
-#    if (NOT MSVC AND NOT ${CMAKE_BUILD_TYPE} MATCHES Debug|RelWithDebInfo)
-#        # Strip unnecessary sections of the binary on Linux/Mac OS
-#        if(CMAKE_STRIP)
-#            if(APPLE)
-#                add_custom_command(TARGET ${target_name} POST_BUILD
-#                                   COMMAND ${CMAKE_STRIP} -x $<TARGET_FILE:${target_name}>)
-#            else()
-#                add_custom_command(TARGET ${target_name} POST_BUILD
-#                                   COMMAND ${CMAKE_STRIP} $<TARGET_FILE:${target_name}>)
-#            endif()
-#        endif()
-#    endif()
-#
-#    if(MSVC)
-#        # /MP enables multithreaded builds (relevant when there are many files), /bigobj is
-#        # needed for bigger binding projects due to the limit to 64k addressable sections
-#        target_compile_options(${target_name} PRIVATE /bigobj)
-#        if(CMAKE_VERSION VERSION_LESS 3.11)
-#            target_compile_options(${target_name} PRIVATE $<$<NOT:$<CONFIG:Debug>>:/MP>)
-#        else()
-#            # Only set these options for C++ files.  This is important so that, for
-#            # instance, projects that include other types of source files like CUDA
-#            # .cu files don't get these options propagated to nvcc since that would
-#            # cause the build to fail.
-#            target_compile_options(${target_name} PRIVATE $<$<NOT:$<CONFIG:Debug>>:$<$<COMPILE_LANGUAGE:CXX>:/MP>>)
-#        endif()
-#    endif()
 endfunction()
