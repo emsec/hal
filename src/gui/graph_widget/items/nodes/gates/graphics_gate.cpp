9a9805cc14586e3f5d510099a0e2b933ac0e80e6
Slowpoke42 <48594426+Slowpoke42@users.noreply.github.com>
2020-02-24 17:24:38 -0800

    state

--- src/gui/graph_widget/items/nodes/gates/graphics_gate.cpp
+++ src/gui/graph_widget/items/nodes/gates/graphics_gate.cpp
@@ -2,8 +2,6 @@
 
 #include "netlist/gate.h"
 
-#include "gui/graph_widget/graph_widget_constants.h"
-
 graphics_gate::graphics_gate(const std::shared_ptr<const gate> g) : graphics_node(hal::item_type::gate, g->get_id(), QString::fromStdString(g->get_name())),
     m_type(QString::fromStdString(g->get_type()->get_name()))
 {
--- src/gui/graph_widget/items/nodes/modules/standard_graphics_module.cpp
+++ src/gui/graph_widget/items/nodes/modules/standard_graphics_module.cpp
@@ -1,7 +1,5 @@
 #include "gui/graph_widget/items/nodes/modules/standard_graphics_module.h"
 
-#include "core/log.h"
-
 #include "netlist/gate.h"
 
 #include "gui/graph_widget/graph_widget_constants.h"
@@ -196,11 +194,7 @@ QPointF standard_graphics_module::get_input_scene_position(const u32 net_id, con
 
     int index = m_input_pins.indexOf(module_pin{net_id, pin_type});
 
-    if (index == -1)
-    {
-        log_error("gui", "input pin type not found.");
-        return mapToScene(QPointF(0, 0));
-    }
+    assert(index != -1);
 
     qreal y = s_color_bar_height + s_pin_upper_vertical_spacing;
 
@@ -218,11 +212,7 @@ QPointF standard_graphics_module::get_output_scene_position(const u32 net_id, co
 
     int index = m_output_pins.indexOf(module_pin{net_id, pin_type});
 
-    if (index == -1)
-    {
-        log_error("gui", "output pin type not found.");
-        return mapToScene(QPointF(0, 0));
-    }
+    assert(index != -1);
 
     qreal y = s_color_bar_height + s_pin_upper_vertical_spacing;
 
