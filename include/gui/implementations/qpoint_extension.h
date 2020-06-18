3e21e353cbf9ebb4000bca2e7ce19b04941195c9
Sebastian Maassen <sebastian.maassen@rub.de>
2020-06-18 19:19:09 +0200

    removed namespace from qpoint extension

--- include/gui/implementations/qpoint_extension.h
+++ include/gui/implementations/qpoint_extension.h
@@ -2,18 +2,17 @@
 #define QPOINT_EXTENSION_H
 
 #include <QPoint>
-//namespace hal{
-//static bool operator<(const QPoint& p1, const QPoint& p2)
-//{
-//    if (p1.x() < p2.x())
-//        return true;
-//    else if (p2.x() < p1.x())
-//        return false;
-//    else if (p1.y() < p2.y())
-//        return true;
-//    else
-//        return false;
-//}
-//}
+static bool operator<(const QPoint& p1, const QPoint& p2)
+{
+    if (p1.x() < p2.x())
+        return true;
+    else if (p2.x() < p1.x())
+        return false;
+    else if (p1.y() < p2.y())
+        return true;
+    else
+        return false;
+}
+
 
 #endif // QPOINT_EXTENSION_H
