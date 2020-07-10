c42e399926af1fc0d28e946922ed2cf0a5240c3f
SJulianS <julian.speith@ruhr-uni-bochum.de>
2020-07-10 16:22:19 +0200

    liberty parser fixes and updated UNISIM library

--- src/netlist/gate_library/gate_library_parser/gate_library_parser_liberty.cpp
+++ src/netlist/gate_library/gate_library_parser/gate_library_parser_liberty.cpp
@@ -410,7 +410,7 @@ namespace hal
 
         pin.direction = direction;
 
-        do
+        while (pin_str.remaining() > 0)
         {
             auto next_token = pin_str.consume();
             if (next_token == "direction")
@@ -453,7 +453,7 @@ namespace hal
                 pin.z_function = pin_str.consume().string;
                 pin_str.consume(";", true);
             }
-        } while (pin_str.remaining() > 0);
+        }
 
         return pin;
     }
