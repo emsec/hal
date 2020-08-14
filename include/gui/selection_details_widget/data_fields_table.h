6a7190408716e194e8b4d0d273ef2c9ae56b4a5a
SJulianS <julian.speith@gmail.com>
2020-08-14 12:11:00 +0200

    license

--- include/gui/selection_details_widget/data_fields_table.h
+++ include/gui/selection_details_widget/data_fields_table.h
@@ -1,30 +1,51 @@
+//  MIT License
+//
+//  Copyright (c) 2019 Ruhr-University Bochum, Germany, Chair for Embedded Security. All Rights reserved.
+//  Copyright (c) 2019 Marc Fyrbiak, Sebastian Wallat, Max Hoffmann ("ORIGINAL AUTHORS"). All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
 #pragma once
 
 #include "def.h"
-#include <QTableWidget>
-#include <QFont>
 #include "selection_details_widget/details_widget.h"
 
-namespace hal {
+#include <QFont>
+#include <QTableWidget>
 
+namespace hal
+{
     class DataContainer;
 
     class DataFieldsTable : public QTableWidget
     {
         Q_OBJECT
     public:
-
         DataFieldsTable(DetailsWidget* parent = nullptr);
-        void updateData(const u32 id, const std::map<std::tuple<std::string, std::string>,
-                                                     std::tuple<std::string, std::string>>& dc);
+        void updateData(const u32 id, const std::map<std::tuple<std::string, std::string>, std::tuple<std::string, std::string>>& dc);
 
     private Q_SLOTS:
-        void handleContextMenuRequest(const QPoint &pos);
+        void handleContextMenuRequest(const QPoint& pos);
 
     private:
-        u32     m_itemId;
+        u32 m_itemId;
         QString m_pythonType;
-        QFont   m_keyFont;
+        QFont m_keyFont;
     };
-}
-
+}    // namespace hal
--- include/gui/selection_widget/models/net_table_model.h
+++ include/gui/selection_widget/models/net_table_model.h
@@ -1,8 +1,31 @@
+//  MIT License
+//
+//  Copyright (c) 2019 Ruhr-University Bochum, Germany, Chair for Embedded Security. All Rights reserved.
+//  Copyright (c) 2019 Marc Fyrbiak, Sebastian Wallat, Max Hoffmann ("ORIGINAL AUTHORS"). All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
 #pragma once
 
 #include "def.h"
 
-#include<QAbstractTableModel>
+#include <QAbstractTableModel>
 
 namespace hal
 {
@@ -13,7 +36,7 @@ namespace hal
     public:
         net_table_model(QObject* parent = 0);
         int rowCount(const QModelIndex& parent = QModelIndex()) const override;
-        int columnCount(const QModelIndex & parent = QModelIndex()) const override;
+        int columnCount(const QModelIndex& parent = QModelIndex()) const override;
         QVariant data(const QModelIndex& inddex, int role = Qt::DisplayRole) const override;
         QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
 
@@ -22,4 +45,4 @@ namespace hal
     private:
         QVector<u32> m_selected_net_ids;
     };
-}
+}    // namespace hal
