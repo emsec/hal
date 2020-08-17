84ae6840a570089ae9a2570b11679d5aa4a704f4
Sebastian Maassen <sebastian.maassen@rub.de>
2020-08-17 21:41:26 +0200

    implemented basic select_gate decorator

--- tools/pydecorator_gui.py
+++ tools/pydecorator_gui.py
@@ -1,31 +1,38 @@
-from functools import wraps
-import hal_py
-from hal_gui import gui
+if "__decorated_gui__" not in dir():
+    __decorated_gui__ = True
 
-# decorates select_gate, overloads: gate-id, gate, list-gate-ids, list-gates
-def select_gate_decorator(message, f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        result = f(*args, **kwargs)
-        if isinstance(args[0], list):
-            print("we have a list!")
-        else:
-            print("it is not a list!")
-        #print("GUI-Function: {}, {}-ID: {}".format(message, type, str(args[0])))
-        return result
-    return decorated
+    import hal_gui
+    from functools import wraps
 
-# different types: gate, net, module, Gate-List, Net-List, Module-List, (gate-ids,net-ids,module-ids), (gate-list,net-list,module-list)
-# "second param always clear_current_selection
-def select_overload_decorator(message, f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        result = f(*args, **kwargs)
-        print("GUI-Function: {}, {}-ID: {}".format(message, type, str(args[0])))
-        return result
-    return decorated
+    # select_gate decorator, possible arguments: [gate-id, list of gate-ids, gate, list of gates,] , bool clear_curr_selec, bool nav_to_selec
+    def select_gate_decorator(message, f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            result = f(*args, **kwargs)
+            log_string = "Function: " + message + ", Gate-ID(s): {"
+            
+            if isinstance(args[1], list):
+                if isinstance(args[1][0], int):
+                    sorted_list = sorted(args[1])
+                    log_string += "".join([str(id) + ", " for id in sorted_list])[:-2] + "}"
+                else:
+                    sorted_list = sorted(args[1], key=lambda gate: gate.id)
+                    log_string += "".join([str(g.id) + ", " for g in sorted_list])[:-2] + "}"
+            else: 
+                if isinstance(args[1], int):
+                    log_string += str(args[1]) + "}"
+                else:
+                    log_string += str(args[1].id) + "}"
 
-gui.select_gate = select_gate_decorator("select_gate", gui.select_gate)
-#gui.select_net = select_decorator("select_net", "Net", gui.select_net)
-#gui.select_module = select_decorator("select_module", "Module", gui.select_module)
-gui.select = select_overload_decorator("test", gui.select)
+            if len(kwargs) > 0:
+                log_string += "(keyword-arguments found, these are not supported yet!"
+            print(log_string)
+            return result
+        return decorated
+
+
+    hal_gui.GuiApi.select_gate = select_gate_decorator("GuiApi.select_gate", hal_gui.GuiApi.select_gate)
+
+else:
+    hal_py.log_info("Gui slready decorated. Not applying again.")
+
