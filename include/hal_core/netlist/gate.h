bde48f961f7f3c6477b6a5181f3d0583bacd82ef
Fabian Albert <Fabian.Albert@rub.de>
2020-10-19 19:47:14 +0200

    documentation fix for gate class

--- include/hal_core/netlist/gate.h
+++ include/hal_core/netlist/gate.h
@@ -55,9 +55,9 @@ namespace hal
     {
     public:
         /**
-         * Gets the unique id of the gate.
+         * Get the unique id of the gate.
          *
-         * @returns The gate's id.
+         * @returns The unique id.
          */
         u32 get_id() const;
 
@@ -69,35 +69,35 @@ namespace hal
         Netlist* get_netlist() const;
 
         /**
-         * Gets the gate's name.
+         * Get the name of the gate.
          *
          * @returns The name.
          */
         std::string get_name() const;
 
         /**
-         * Sets the gate's name.
+         * Set the name of the gate.
          *
          * @param[in] name - The new name.
          */
         void set_name(const std::string& name);
 
         /**
-         * Gets the type of the gate.
+         * Get the type of the gate.
          *
-         * @returns The gate's type.
+         * @returns The type.
          */
         const GateType* get_type() const;
 
         /**
-         * Checks whether the gate's location in the layout is available.
+         * Check whether the gate's location in the layout is available.
          *
          * @returns True if valid location data is available, false otherwise.
          */
         bool has_location() const;
 
         /**
-         * Sets the physical location x-coordinate of the gate in the layout.
+         * Set the physical location x-coordinate of the gate in the layout.<br>
          * Only positive values are valid, negative values will be regarded as no location assigned.
          *
          * @param[in] x - The gate's x-coordinate.
@@ -105,14 +105,15 @@ namespace hal
         void set_location_x(float x);
 
         /**
-         * Gets the physical location x-coordinate of the gate in the layout.
+         * Get the physical location x-coordinate of the gate in the layout.<br>
+         * If no valid physical location x-coordinate is assigned, a negative value is returned.
          *
          * @returns The gate's x-coordinate.
          */
         float get_location_x() const;
 
         /**
-         * Sets the physical location y-coordinate of the gate in the layout.
+         * Set the physical location y-coordinate of the gate in the layout.<br>
          * Only positive values are valid, negative values will be regarded as no location assigned.
          *
          * @param[in] y - The gate's y-coordinate.
@@ -120,35 +121,40 @@ namespace hal
         void set_location_y(float y);
 
         /**
-         * Gets the physical location y-coordinate of the gate in the layout.
+         * Get the physical location y-coordinate of the gate in the layout.<br>
+         * If no valid physical location y-coordinate is assigned, a negative value is returned.
          *
          * @returns The gate's y-coordinate.
          */
         float get_location_y() const;
 
         /**
-         * Sets the physical location of the gate in the layout.
+         * Set the physical location of the gate in the layout.<br>
+         * Only positive coordinates are valid, negative values will be regarded as no location assigned.
          *
          * @param[in] location - A pair <x-coordinate, y-coordinate>.
          */
         void set_location(const std::pair<float, float>& location);
 
         /**
-         * Gets the physical location of the gate in the layout.
+         * Get the physical location of the gate in the layout.<br>
+         * If no valid physical location coordinate is assigned, a negative value is returned for the
+         * respective coordinate.
          *
          * @returns A pair <x-coordinate, y-coordinate>.
          */
         std::pair<float, float> get_location() const;
 
         /**
-         * Gets the module in which this gate is contained.
+         * Get the module which contains this gate.
          *
          * @returns The module.
          */
         Module* get_module() const;
 
         /**
-         * Gets the grouping in which this gate is contained.
+         * Gets the grouping in which this gate is contained.<br>
+         * If no grouping contains this gate, a nullptr is returned.
          *
          * @returns The grouping.
          */
@@ -156,8 +162,8 @@ namespace hal
 
         /**
          * Get the boolean function associated with a specific name.
-         * This name can for example be an output pin of the gate or a defined functionality like "reset".
-         * If name is empty, the function of the first output pin is returned.
+         * This name can for example be an output pin of the gate or a defined functionality like "reset".<br>
+         * If name is empty, the function of the first output pin is returned.<br>
          * If there is no function for the given name, an empty function is returned.
          *
          * @param[in] name - The function name.
@@ -242,21 +248,23 @@ namespace hal
         std::vector<std::string> get_output_pins() const;
 
         /**
-         * Get a set of all fan-in nets of the gate, i.e. all nets that are connected to one of the input pins.
+         * Get a list of all fan-in nets of the gate, i.e. all nets that are connected to one of the input pins.
          *
-         * @returns A set of all connected input nets.
+         * @returns A vector of all connected input nets.
          */
         std::vector<Net*> get_fan_in_nets() const;
 
         /**
-         * Get a all fan-in endpoint of the gate, i.e. all nets that are connected to one of the input pins + the respective pin.
+         * Get a list of all fan-in endpoints of the gate,
+         * i.e. all connected endpoints that represent an input pin of this gate.
          *
-         * @returns A set of all connected input endpoints.
+         * @returns A vector of all connected input endpoints.
          */
         std::vector<Endpoint*> get_fan_in_endpoints() const;
 
         /**
-         * Get the fan-in net which is connected to a specific input pin.
+         * Get the fan-in net which is connected to a specific input pin. <br>
+         * If the input pin type is unknown or no net is connected, a nullptr is returned.
          *
          * @param[in] pin - The input pin type.
          * @returns The connected input net.
@@ -264,7 +272,8 @@ namespace hal
         Net* get_fan_in_net(const std::string& pin) const;
 
         /**
-         * Get the fan-in endpoint which is connected to a specific input pin.
+         * Get the fan-in endpoint which represents a specific input pin. <br>
+         * If the input pin type is unknown or no net is connected to the respective pin, a nullptr is returned.
          *
          * @param[in] pin - The input pin type.
          * @returns The endpoint.
@@ -272,21 +281,23 @@ namespace hal
         Endpoint* get_fan_in_endpoint(const std::string& pin) const;
 
         /**
-         * Get a set of all fan-out nets of the gate, i.e. all nets that are connected to one of the output pins.
+         * Get a list of all fan-out nets of the gate, i.e. all nets that are connected to one of the output pins.
          *
-         * @returns A set of all connected output nets.
+         * @returns A vector of all connected output nets.
          */
         std::vector<Net*> get_fan_out_nets() const;
 
         /**
-         * Get a all fan-out endpoint of the gate, i.e. all nets that are connected to one of the output pins + the respective pin.
+         * Get a list of all fan-out endpoints of the gate,
+         * i.e. all connected endpoints that represent an output pin of this gate.
          *
-         * @returns A set of all connected output endpoints.
+         * @returns A vector of all connected output endpoints.
          */
         std::vector<Endpoint*> get_fan_out_endpoints() const;
 
         /**
-         * Get the fan-out net which is connected to a specific output pin.
+         * Get the fan-out net which is connected to a specific output pin. <br>
+         * If the output pin type is unknown or no net is connected, a nullptr is returned.
          *
          * @param[in] pin - The output pin type.
          * @returns The connected output net.
@@ -294,7 +305,8 @@ namespace hal
         Net* get_fan_out_net(const std::string& pin) const;
 
         /**
-         * Get the fan-out endpoint which is connected to a specific output pin.
+         * Get the fan-out endpoint which represents a specific output pin. <br>
+         * If the input pin type is unknown or no net is connected to the respective pin, a nullptr is returned.
          *
          * @param[in] pin - The output pin type.
          * @returns The endpoint.
@@ -302,53 +314,62 @@ namespace hal
         Endpoint* get_fan_out_endpoint(const std::string& pin) const;
 
         /**
-         * Get a set of all unique predecessor gates of the gate.
+         * Get a list of all unique predecessor gates of the gate. <br>
          * A filter can be supplied which filters out all potential values that return false.
          *
-         * @param[in] filter - a function to filter the output. Leave empty for no filtering.
+         * @param[in] filter - A function to filter the output, using the input pin type of the gate (1st param) and
+         *                     a connected predecessor endpoint (2nd param). Leave empty for no filtering.
          * @returns A vector of unique predecessor gates.
          */
         std::vector<Gate*> get_unique_predecessors(const std::function<bool(const std::string& starting_pin, Endpoint* ep)>& filter = nullptr) const;
 
         /**
-         * Get a vector of all direct predecessor endpoints of the gate.
+         * Get a list of all direct predecessor endpoints of the gate,
+         * i.e. all output endpoints that are connected to an input pin of the gate. <br>
          * A filter can be supplied which filters out all potential values that return false.
          *
-         * @param[in] filter - a function to filter the output. Leave empty for no filtering.
+         * @param[in] filter - A function to filter the output, using the input pin type of the gate (1st param) and
+         *                     a connected predecessor endpoint (2nd param). Leave empty for no filtering.
          * @returns A vector of predecessor endpoints.
          */
         std::vector<Endpoint*> get_predecessors(const std::function<bool(const std::string& starting_pin, Endpoint* ep)>& filter = nullptr) const;
 
         /**
-         * Get the direct predecessor endpoint of the gate connected to a specific input pin.
+         * Get the direct predecessor endpoint of the gate connected to a specific input pin. <br>
+         * If the input pin type is unknown or there is no predecessor endpoint or there are multiple predecessor
+         * endpoints, a nullptr is returned.
          *
-         * @param[in] which_pin - the pin of this gate to get the predecessor from.
+         * @param[in] which_pin - the input pin type of this gate to get the predecessor from.
          * @returns The predecessor endpoint.
          */
         Endpoint* get_predecessor(const std::string& which_pin) const;
 
         /**
-         * Get a set of all unique successor gates of the gate.
+         * Get a list of all unique successor gates of the gate. <br>
          * A filter can be supplied which filters out all potential values that return false.
          *
-         * @param[in] filter - a function to filter the output. Leave empty for no filtering.
+         * @param[in] filter - A function to filter the output, using the output pin type of the gate (1st param) and
+         *                     a connected successor endpoint (2nd param). Leave empty for no filtering.
          * @returns A vector of unique successor gates.
          */
         std::vector<Gate*> get_unique_successors(const std::function<bool(const std::string& starting_pin, Endpoint* ep)>& filter = nullptr) const;
 
         /**
-         * Get a vector of all direct successor endpoints of the gate.
+         * Get a list of all direct successor endpoints of the gate. <br>
          * A filter can be supplied which filters out all potential values that return false.
          *
-         * @param[in] filter - a function to filter the output. Leave empty for no filtering.
+         * @param[in] filter - A function to filter the output, using the output pin type of the gate (1st param) and
+         *                     a connected successor endpoint (2nd param). Leave empty for no filtering.
          * @returns A vector of successor endpoints.
          */
         std::vector<Endpoint*> get_successors(const std::function<bool(const std::string& starting_pin, Endpoint* ep)>& filter = nullptr) const;
 
         /**
-         * Get the direct successor endpoint of the gate connected to a specific input pin.
+         * Get the direct successor endpoint of the gate connected to a specific input pin. <br>
+         * If the input pin type is unknown or there is no successor endpoint or there are multiple successor
+         * endpoints, a nullptr is returned.
          *
-         * @param[in] which_pin - the pin of this gate to get the successor from.
+         * @param[in] which_pin - The output pin type of this gate to get the successor from.
          * @returns The successor endpoint.
          */
         Endpoint* get_successor(const std::string& which_pin) const;
