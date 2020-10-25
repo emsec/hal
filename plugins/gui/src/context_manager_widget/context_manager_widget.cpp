41fe7cbdd1734a9619943be2f143c7f0af3884c5
Tobias Hermanns <tobias.hermanns@rub.de>
2020-10-25 02:09:27 +0100

    Change action for wiki continuity

--- plugins/gui/src/context_manager_widget/context_manager_widget.cpp
+++ plugins/gui/src/context_manager_widget/context_manager_widget.cpp
@@ -171,8 +171,8 @@ namespace hal
         if (clicked_index.isValid())
         {
             context_menu.addAction(m_open_action);
-            context_menu.addAction(m_rename_action);
             context_menu.addAction(m_duplicate_action);
+            context_menu.addAction(m_rename_action);
             context_menu.addAction(m_delete_action);
         }
 
