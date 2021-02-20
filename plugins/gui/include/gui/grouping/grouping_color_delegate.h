7f77c0a08ce4ef2d500d184798710f5fb4be134a
Fabian Albert <Fabian.Albert@rub.de>
2021-02-20 19:44:18 +0100

    +Docu: gui grouping

--- plugins/gui/include/gui/grouping/grouping_color_delegate.h
+++ plugins/gui/include/gui/grouping/grouping_color_delegate.h
@@ -3,15 +3,37 @@
 #include <QStyledItemDelegate>
 
 namespace hal {
+    /**
+     * QStyledItemDelegate used for displaying a colored box in the 'Color'-column
+     * within the grouping_manager_widget table.
+     */
     class GroupingColorDelegate : public QStyledItemDelegate
     {
         Q_OBJECT
-
-
     public:
+        /**
+         * Constructor.
+         *
+         * @param parent - The parent widget
+         */
         GroupingColorDelegate(QObject *parent = 0) : QStyledItemDelegate(parent) {;}
 
+        /**
+         * Paints the colored box in the table cell.
+         *
+         * @param painter - The used painter
+         * @param option - The used style options
+         * @param index - The index of the grouping within the GroupingTableModel
+         */
         void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+
+        /**
+         * Returns the sized needed by the delegate to display the colored box.
+         *
+         * @param option - The used style options
+         * @param index - The index of the grouping within the GroupingTableModel
+         * @returns he sized needed by the delegate to display the colored box
+         */
         QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const override;
     };
 }
--- plugins/gui/include/gui/grouping/grouping_table_model.h
+++ plugins/gui/include/gui/grouping/grouping_table_model.h
@@ -28,25 +28,99 @@ namespace hal {
         Grouping* grouping() const { return mGrouping; }
     };
 
-
+    /**
+     * The table model used for groupings within the GroupingsManagerWidget. <br>
+     * Read the Qt documentation for further descriptions of the table model system.
+     */
     class GroupingTableModel : public QAbstractTableModel, public Validator
     {
         Q_OBJECT
 
         bool mDisableEvents;
+
         QList<GroupingTableEntry> mGroupings;
         QString mAboutToRename;
 
         static QString generateUniqueName(const QString& suggestion, const QSet<QString>& existingNames);
     public:
+        /**
+         * Constructor.
+         *
+         * @param parent - The parent widget
+         */
         GroupingTableModel(QObject* parent=nullptr);
+
+        /**
+         * Returns the amount of columns. Is always 3 (Grouping, ID, Color)
+         *
+         * @param parent - Then parent model index
+         * @returns 3
+         */
         int columnCount(const QModelIndex &parent=QModelIndex()) const override;
+
+        /**
+         * Returns the amount of rows in the table. Is equal to the amount of stored groupings.
+         *
+         * @param parent - The parent model index
+         * @returns the amount of rows in the table.
+         */
         int rowCount(const QModelIndex &parent=QModelIndex()) const override;
+
+        /**
+         * Returns the data stored under the given role for a given index in the table model.
+         *
+         * @param index - The index in the table
+         * @param role - The access role
+         * @returns the data
+         */
         QVariant data(const QModelIndex &index, int role) const override;
+
+        /**
+         * Returns the header data fields, i.e. 'Groupings', 'ID' or 'Color'
+         *
+         * @param section - The section (column) index
+         * @param orientation - The orientation of the table
+         * @param role - The access role
+         * @returns the header data field at the given position
+         */
         QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+
+        /**
+         * Sets the role data for the item at index to value.
+         *
+         * @param index - The model index
+         * @param value - The value to set
+         * @param role - The access role
+         * @returns <b>true</b> on success
+         */
         bool setData(const QModelIndex &index, const QVariant &value, int role) override;
+
+        /**
+         * Remove the row at index row.
+         *
+         * @param row - The row index
+         * @param count - Unused and ignored (always one row is deleted at a time)
+         * @param parent - Unused and ingnored
+         * @returns <b>true</b> on success
+         */
         bool removeRows(int row, int count=1, const QModelIndex &parent=QModelIndex()) override;
+
+        /**
+         * Returns <b>true</b> if <i>input</i> is a unique and valid grouping name. Returns <b>false</b> otherwise.
+         *
+         * @param input - A grouping name
+         * @returns <b>true</b> if <i>input</i> is a unique and valid grouping name.
+         *          Returns <b>false</b> otherwise
+         */
         bool validate(const QString &input);
+
+        /**
+         * This function is used to prevent the 'validate'-function to return <b>false</b> while renaming a grouping to
+         * its old name. Therefore the old name of the grouping will always considered as valid. <br>
+         * To achieve this one has to call this function to store the old name.
+         *
+         * @param oldName - The old name of the grouping
+         */
         void setAboutToRename(const QString& oldName) { mAboutToRename = oldName.trimmed(); }
         GroupingTableEntry groupingAt(int irow) const { return mGroupings.at(irow); }
         Grouping* addDefaultEntry();
