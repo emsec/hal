afc68d5dbc8381459372cf1646768f0f249f59b7
Fabian Albert <Fabian.Albert@rub.de>
2021-03-24 16:12:59 +0100

    Docu: ContentLayoutArea

--- plugins/gui/include/gui/content_layout_area/content_layout_area.h
+++ plugins/gui/include/gui/content_layout_area/content_layout_area.h
@@ -46,27 +46,102 @@ namespace hal
         bottom = 3
     };
 
+    /**
+     * The ContentLayoutArea manages the main layout of the content widgets that appears after a file was opened.
+     * It owns the hierarchy of the layout down to the Splitters/SplitterAnchors. In provides a function to move
+     * content widgets to one of its content anchors.
+     *
+     * The ContentLayoutArea builds up the following layout hierarchy to achieve the desired
+     * overall layout (in this order): <br>
+     * <i>mTopLevelLayout</i> <ul>
+     *   <li> <i>mSecondLevelLayout</i> <ul>
+     *      <li> mLeftDock (the docker bar on the far left)
+     *      <li> <i>mThirdLevelLayout</i> <ul>
+     *          <li> <i>mFourthLevelLayout</i> (TODO: 3rd and 4th level layout unnecessary?) <ul>
+     *              <li> <i>mVerticalSplitter</i> <ul>
+     *                  <li> <i>mHorizontalSplitter</i> <ul>
+     *                      <li> mLeftSplitter (contains the content widgets docked and opened left)
+     *                      <li> mTabWidget (the tab widget (including the top docker bar) at the center that only
+     *                           contains the graph-views widget)
+     *                      <li> mRightSplitter (contains the content widgets docked and opened right) </ul>
+     *                  <li> mBottomSplitter(contains the content widgets docked and opened at the bottom) </ul> </ul> </ul>
+     *      <li> mRightDock (the docker bar on the far right) </ul>
+     *   <li> <i>mBottomContainer</i> <ul>
+     *      <li> mLeftSpacer (an empty frame to create a margin at the left side of the bottom docker)
+     *      <li> mBottomDock (the docker bar at the very bottom)
+     *      <li> mRightSpacer (an empty frame to create a margin at the right side of the bottom docker) </ul> </ul>
+     */
     class ContentLayoutArea : public QWidget
     {
         Q_OBJECT
 
     public:
+        /**
+         * Constructor.
+         * Already builds up the layout hierarchy.
+         *
+         * @param parent - The parent widget
+         */
         explicit ContentLayoutArea(QWidget* parent = nullptr);
 
+        /**
+         * Adds a content widget to the a specified content anchor at a specified position.
+         *
+         * @param widget - The content widget that should be added to the anchor
+         * @param index - The position index this widget should have in the anchor it is moved to
+         * @param anchor - The anchor the content widget should be moved to (i.e. the direction of the docker bar it
+         *                 is moved to)
+         */
         void addContent(ContentWidget* widget, int index, content_anchor anchor);
+
+        /**
+         * Not yet implemented. TODO: Remove me?/Disable me?
+         *
+         * @param widget
+         */
         void removeContent(ContentWidget* widget);
 
+        /**
+         * Removes all buttons from all docker bars and therefore all widgets.
+         * Mark that the widgets are not destroyed but only hidden.
+         */
         void clear();
 
+        /**
+         * Given the size of the MainWindow this function initializes the splitter sizes of the various splitters in the
+         * layout hierarchy. They are chosen to provide reasonable proportions for default usage.
+         *
+         * @param size
+         */
         void initSplitterSize(const QSize& size);
 
     public Q_SLOTS:
+        /**
+         * Q_SLOT to update the appearance of the left docker bar. If the docker bar contains no widget it will be hidden.
+         */
         void updateLeftDockBar();
+
+        /**
+         * Q_SLOT to update the appearance of the right docker bar. If the docker bar contains no widget it will be hidden.
+         */
         void updateRightDockBar();
+
+        /**
+         * Q_SLOT to update the appearance of the bottom docker bar. If the docker bar contains no widget it will be hidden.
+         */
         void updateBottomDockBar();
 
-        //needed to fix a bug where the bottom dockbar does not reappear after dragging every widget out of it
+        /**
+         * TODO (old): needed to fix a bug where the bottom dockbar does not reappear after dragging every widget out of it
+         * Q_SLOT to handle that a docker button was dragged out of its bar. All hidden docker bars are temporarily shown
+         * while dragging a docker button.
+         */
         void handleDragStart();
+
+        /**
+         * Q_SLOT to handle the end of the docker button drag event (see handleDragStart). All hidden docker bars that
+         * were temporarily shown while dragging a docker button are hidden again.
+         */
         void handleDragEnd();
 
     private:
