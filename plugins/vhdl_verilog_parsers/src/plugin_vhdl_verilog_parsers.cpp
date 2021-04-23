9a80474272745425f7d33f105cda17bad6c09764
SJulianS <julian.speith@gmail.com>
2021-04-23 11:11:12 +0200

    cleanup

--- plugins/vhdl_verilog_parsers/src/plugin_vhdl_verilog_parsers.cpp
+++ plugins/vhdl_verilog_parsers/src/plugin_vhdl_verilog_parsers.cpp
@@ -1,7 +1,6 @@
 #include "vhdl_verilog_parsers/plugin_vhdl_verilog_parsers.h"
 
 #include "hal_core/netlist/netlist_parser/netlist_parser_manager.h"
-#include "vhdl_verilog_parsers/hdl_parser_verilog.h"
 #include "vhdl_verilog_parsers/hdl_parser_vhdl.h"
 
 namespace hal
@@ -23,13 +22,11 @@ namespace hal
 
     void VhdlVerilogParsersPlugin::on_load()
     {
-        // netlist_parser_manager::register_parser("Default Verilog Parser", [](){return std::make_unique<HDLParserVerilog>();}, {".v"});
         netlist_parser_manager::register_parser("Default VHDL Parser", []() { return std::make_unique<HDLParserVHDL>(); }, {".vhd", ".vhdl"});
     }
 
     void VhdlVerilogParsersPlugin::on_unload()
     {
-        netlist_parser_manager::unregister_parser("Default Verilog Parser");
         netlist_parser_manager::unregister_parser("Default VHDL Parser");
     }
 }    // namespace hal
