c0d165f393c954346d66d49c8e3f92b6fd8f9f4a
SJulianS <julian.speith@gmail.com>
2021-04-28 14:06:24 +0200

    updated licenses (except GUI)

--- plugins/z3_utils/include/converter/cpp_converter.h
+++ plugins/z3_utils/include/converter/cpp_converter.h
@@ -1,14 +1,38 @@
+//  MIT License
+//
+//  Copyright (c) 2019 Ruhr University Bochum, Chair for Embedded Security. All Rights reserved.
+//  Copyright (c) 2021 Max Planck Institute for Security and Privacy. All Rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
 #pragma once
 
+#include "converter/converter.h"
 #include "hal_core/defines.h"
 
-#include "converter/converter.h"
-
-namespace hal {
-    namespace z3_utils 
+namespace hal
+{
+    namespace z3_utils
     {
-        class Cpp_Converter : public Converter{
-            private:
+        class Cpp_Converter : public Converter
+        {
+        private:
             // VIRTUAL METHODS
             std::string build_operand(const std::string& operand) const override;
             std::string build_operation(const Operation& operation, const std::vector<std::string>& operands) const override;
@@ -17,9 +41,9 @@ namespace hal {
             std::string construct_function(const std::string& assignments, const std::string& initalization, const std::vector<u32>& inputs) const override;
 
             std::string m_function_corpus = R"(
+    #include <stdbool.h> 
     #include <stdio.h>
     #include <stdlib.h>
-    #include <stdbool.h> 
 
     static unsigned long x=123456789, y=362436069, z=521288629;
 
@@ -84,5 +108,5 @@ namespace hal {
         return count;
     })";
         };
-    }  //namespace z3_utils
-}   // namespace hal
+    }    //namespace z3_utils
+}    // namespace hal
