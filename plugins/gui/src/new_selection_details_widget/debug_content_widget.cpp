9a4d09a8193f5b2ec640fbf5dbbd4a4a2045dc34
Fabian Albert <Fabian.Albert@rub.de>
2021-07-29 16:19:59 +0200

    added scrollbar to temporary debug content widget (ugly and proud)

--- plugins/gui/src/new_selection_details_widget/debug_content_widget.cpp
+++ plugins/gui/src/new_selection_details_widget/debug_content_widget.cpp
@@ -16,49 +16,77 @@
 #include "gui/new_selection_details_widget/new_module_details_widget/module_info_table.h"
 #include "gui/new_selection_details_widget/data_table_widget.h"
 #include "gui/new_selection_details_widget/groupings_of_item_widget.h"
+#include <QScrollArea>
+#include <QGridLayout>
 
 namespace hal {
     DebugContentWidget::DebugContentWidget(QWidget *parent) : ContentWidget("Debug Widget", parent)
     {
-        //mContentLayout->addWidget(new DataTableWidget(this));
-        //mContentLayout->addWidget(new GroupingsOfItemWidget(this));
+        QGridLayout *scrollAreaLayout = new QGridLayout(this);
+        
+        QWidget* scrollAreaContent = new QWidget(this);
+        scrollAreaContent->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        
+        scrollAreaContent->setLayout(scrollAreaLayout);
+        scrollAreaLayout->setContentsMargins(0, 0, 0, 0);
+        scrollAreaLayout->setSpacing(30);
+
+        QScrollArea* scrollArea = new QScrollArea(this);
+        scrollArea->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
+        
+        scrollArea->setWidgetResizable( true );
+        scrollArea->setVerticalScrollBarPolicy( Qt::ScrollBarAlwaysOn );
+        scrollArea->setWidget(scrollAreaContent);
+        
+        scrollAreaLayout->setSizeConstraint(QLayout::SetNoConstraint);
+        mContentLayout->addWidget(scrollArea);
+        
+
+        // --- Add your widgets here ---
+
+        scrollAreaLayout->addWidget(new DataTableWidget(this));
+        scrollAreaLayout->addWidget(new GroupingsOfItemWidget(this));
+        scrollAreaLayout->addWidget(new LUTTableWidget(this));
+
 
         //--------For NetlistElementsModel
         //NOTE: I've commented it out temporarily, because it SIGSEGVs sometimes (in netlists without gate id 2)
-//        QTreeView* mTreeView = new QTreeView(this);
-//        mTreeView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-//        mTreeView->header()->setStretchLastSection(true);
-//        NetlistElementsTreeModel* model = new NetlistElementsTreeModel(mTreeView);
-//        BaseTreeModel* baseModel = new BaseTreeModel(mTreeView);
-//        PinTreeModel* pinModel = new PinTreeModel(mTreeView);
-//        PortTreeModel* portModel = new PortTreeModel(mTreeView);
-//        baseModel->setHeaderLabels(QList<QVariant>() << "Label1" << "Label2");
-//        TreeItem* someRootItem = new TreeItem(QList<QVariant>() << "parent1" << "parent2");
-//        TreeItem* someChildItem = new TreeItem(QList<QVariant>() << "Test1" << "Test2");
-//        someRootItem->appendChild(someChildItem);
-//        baseModel->setContent(QList<TreeItem*>() << someRootItem);
-//        mTreeView->setModel(pinModel);
-//        //mTreeView->setModel(model);
-//        mContentLayout->addWidget(mTreeView);
-//        GatePinTree* gp = new GatePinTree(this);
-//        gp->setContent(19);
-//        mContentLayout->addWidget(gp);
+        QTreeView* mTreeView = new QTreeView(this);
+        mTreeView->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        mTreeView->header()->setStretchLastSection(true);
+        NetlistElementsTreeModel* model = new NetlistElementsTreeModel(mTreeView);
+        BaseTreeModel* baseModel = new BaseTreeModel(mTreeView);
+        PinTreeModel* pinModel = new PinTreeModel(mTreeView);
+        PortTreeModel* portModel = new PortTreeModel(mTreeView);
+        baseModel->setHeaderLabels(QList<QVariant>() << "Label1" << "Label2");
+        TreeItem* someRootItem = new TreeItem(QList<QVariant>() << "parent1" << "parent2");
+        TreeItem* someChildItem = new TreeItem(QList<QVariant>() << "Test1" << "Test2");
+        someRootItem->appendChild(someChildItem);
+        baseModel->setContent(QList<TreeItem*>() << someRootItem);
+        mTreeView->setModel(pinModel);
+        //mTreeView->setModel(model);
+        scrollAreaLayout->addWidget(mTreeView);
+        GatePinTree* gp = new GatePinTree(this);
+        gp->setContent(19);
+        scrollAreaLayout->addWidget(gp);
         ModuleElementsTree* met = new ModuleElementsTree(this);
         met->setContent(1);
-        mContentLayout->addWidget(met);
+        
+        scrollAreaLayout->addWidget(met);
 
+        
         //--------End NetlistElementsModel
 
 
         //General Info Tables
-        //GateInfoTable* gateTable = new GateInfoTable(this); //hardcoded to show gate with id 11 in constructor atm
-        //mContentLayout->addWidget(gateTable);
+        GateInfoTable* gateTable = new GateInfoTable(this); //hardcoded to show gate with id 11 in constructor atm
+        scrollAreaLayout->addWidget(gateTable);
 
-        //NetInfoTable* netTable = new NetInfoTable(this); //hardcoded to show net with id 17 in constructor atm
-        //mContentLayout->addWidget(netTable);
+        NetInfoTable* netTable = new NetInfoTable(this); //hardcoded to show net with id 17 in constructor atm
+        scrollAreaLayout->addWidget(netTable);
 
-        //ModuleInfoTable* moduleTable = new ModuleInfoTable(this); //hardcoded to show module with id 1 in constructor atm
-        //mContentLayout->addWidget(moduleTable);
+        ModuleInfoTable* moduleTable = new ModuleInfoTable(this); //hardcoded to show module with id 1 in constructor atm
+        scrollAreaLayout->addWidget(moduleTable);
         //-----------------------------------------------------------------------------------------------------------------------
     }
 
