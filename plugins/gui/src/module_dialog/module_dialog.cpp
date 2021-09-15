d5f75ba0f8530e99ae701f93010f77b13bc1f79d
joern274 <joern274@gmail.com>
2021-09-15 21:58:59 +0200

    merge error when manually resolving conflict fixed

--- plugins/gui/src/module_dialog/module_dialog.cpp
+++ plugins/gui/src/module_dialog/module_dialog.cpp
@@ -58,11 +58,6 @@ namespace hal {
 
         layout->addWidget(mTabWidget, 2, 0, 1, 3);
 
-        mModuleTableProxyModel = static_cast<ModuleSelectProxy*>(mTableView->model());
-        mModuleTableProxyModel->setFilterKeyColumn(-1);
-        mModuleTableProxyModel->setDynamicSortFilter(true);
-        mModuleSelectModel = static_cast<ModuleSelectModel*>(mModuleTreeProxyModel->sourceModel());
-
         mModuleTreeProxyModel = new ModuleProxyModel(this);
         mModuleTreeProxyModel->setFilterKeyColumn(-1);
         mModuleTreeProxyModel->setDynamicSortFilter(true);
