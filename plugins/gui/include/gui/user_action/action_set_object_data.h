b8d518be6f7d3e9a19007ac5929ff48cc0e8c7ed
Sebastian Maassen <sebastian.maassen@rub.de>
2022-07-23 11:10:38 -0700

    wip - added basic files for seting/modifying data using user actions (without ability to delete data yet)

---
+++ plugins/gui/include/gui/user_action/action_set_object_data.h
@@ -0,0 +1,75 @@
+//  MIT License
+//
+//  Copyright (c) 2019 Ruhr University Bochum, Chair for Embedded Security. All Rights reserved.
+//  Copyright (c) 2021 Max Planck Institute for Security and Privacy. All Rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+#pragma once
+#include "user_action.h"
+
+namespace hal
+{
+    class ActionSetObjectData : public UserAction
+    {
+
+    public:
+        ActionSetObjectData(QString category = QString(), QString key = QString(), QString type = QString(), QString val = QString());
+
+        bool exec() override;
+        QString tagname() const override;
+        void writeToXml(QXmlStreamWriter& xmlOut) const override;
+        void readFromXml(QXmlStreamReader& xmlIn) override;
+        void addToHash(QCryptographicHash& cryptoHash) const override;
+
+        /**
+         * This function must be called when the key and/or category property of an already existing entry
+         * shall to be modified. Otherwise, instead of changing the 4 tupel from (oldKey, oldCat, oldType, oldVal)
+         * to (newKey, newCat, newType, newVal), a new entry would be inserted and both of them would coexist.
+         * (This also applies if, for example, one wants to only modify the category)
+         *
+         * @param oldKey - The old key that is to "potentionally" overwritten.
+         * @param oldCategory - The old category that is to "potentionally" overwritten.
+         */
+        void setChangeKeyAndOrCategory(QString oldCategory, QString oldKey);
+
+    private:
+        QString mKey;
+        QString mCat;
+        QString mType;
+        QString mVal;
+
+        QString mOldKey;
+        QString mOldCat;
+        bool mKeyOrCatModified;
+    };
+
+    /**
+     * @brief The ActionSetObjectDataFactory class
+     */
+    class ActionSetObjectDataFactory : public UserActionFactory
+    {
+    public:
+        ActionSetObjectDataFactory();
+        UserAction * newAction() const override;
+        static ActionSetObjectDataFactory* sFactory;
+
+    };
+
+}
---
+++ plugins/gui/src/user_action/action_set_object_data.cpp
@@ -0,0 +1,102 @@
+#include "gui/user_action/action_set_object_data.h"
+#include "gui/gui_globals.h"
+
+namespace hal
+{
+
+    ActionSetObjectDataFactory::ActionSetObjectDataFactory() : UserActionFactory("SetObjectData")
+    {}
+    ActionSetObjectDataFactory* ActionSetObjectDataFactory::sFactory = new ActionSetObjectDataFactory;
+
+    UserAction *ActionSetObjectDataFactory::newAction() const
+    {
+        return new ActionSetObjectData;
+    }
+
+    ActionSetObjectData::ActionSetObjectData(QString category, QString key, QString type, QString val)
+    : mKey(key), mCat(category), mType(type), mVal(val), mOldKey(""), mOldCat(""), mKeyOrCatModified(false)
+    {
+    }
+
+    bool ActionSetObjectData::exec()
+    {
+        //for now, this action only supports setting the data of gates, modules and nets (DataContainer)
+        DataContainer* container = nullptr;
+        switch(mObject.type())
+        {
+            case UserActionObjectType::Gate:
+                container = dynamic_cast<DataContainer*>(gNetlist->get_gate_by_id(mObject.id()));
+                break;
+            case UserActionObjectType::Module:
+                container = dynamic_cast<DataContainer*>(gNetlist->get_module_by_id(mObject.id()));
+                break;
+            case UserActionObjectType::Net:
+                container = dynamic_cast<DataContainer*>(gNetlist->get_net_by_id(mObject.id()));
+                break;
+            default: break;
+        }
+
+        if(!container)
+            return false;
+
+        //implicit modifying of an existing entry, but it must be deleted beforehand and undo action must be constructed in an appropriate way
+        if(mKeyOrCatModified)
+        {
+            if(!container->has_data(mOldCat.toStdString(), mOldKey.toStdString()))//additional security check
+                return false;
+
+            //construct appropriate undo action
+            auto data = container->get_data(mOldCat.toStdString(), mOldKey.toStdString());
+            mUndoAction = new ActionSetObjectData(mOldCat, mOldKey, QString::fromStdString(std::get<0>(data)), QString::fromStdString(std::get<1>(data)));
+            mUndoAction->setObject(mObject);
+            static_cast<ActionSetObjectData*>(mUndoAction)->setChangeKeyAndOrCategory(mCat, mKey);
+
+            //delete old, add new
+            container->delete_data(mOldCat.toStdString(), mOldKey.toStdString());
+            container->set_data(mCat.toStdString(), mKey.toStdString(), mType.toStdString(), mVal.toStdString());
+        }
+        //check if the data to set already exists (undo action is then "change back")
+        else if(container->has_data(mCat.toStdString(), mKey.toStdString()))
+        {
+            auto data = container->get_data(mCat.toStdString(), mKey.toStdString());
+            mUndoAction = new ActionSetObjectData(mCat, mKey, QString::fromStdString(std::get<0>(data)), QString::fromStdString(std::get<1>(data)));
+            mUndoAction->setObject(mObject);
+            container->set_data(mCat.toStdString(), mKey.toStdString(), mType.toStdString(), mVal.toStdString());
+        }
+        else //if the data does not already exists -> it is new data -> undo action deletes it
+        {
+
+
+        }
+
+        return UserAction::exec();
+    }
+
+    QString ActionSetObjectData::tagname() const
+    {
+        return ActionSetObjectDataFactory::sFactory->tagname();
+    }
+
+    void ActionSetObjectData::writeToXml(QXmlStreamWriter &xmlOut) const
+    {
+
+    }
+
+    void ActionSetObjectData::readFromXml(QXmlStreamReader &xmlIn)
+    {
+
+    }
+
+    void ActionSetObjectData::addToHash(QCryptographicHash &cryptoHash) const
+    {
+
+    }
+
+    void ActionSetObjectData::setChangeKeyAndOrCategory(QString oldCategory, QString oldKey)
+    {
+        mOldKey = oldKey;
+        mOldCat = oldCategory;
+        mKeyOrCatModified = true;
+    }
+
+}
