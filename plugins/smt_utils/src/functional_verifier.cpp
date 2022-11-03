ba350c374d2e514a9eea51e506fa9be2244f189c
SJulianS <julian.speith@gmail.com>
2022-11-03 17:43:30 +0100

    state

--- plugins/smt_utils/src/functional_verifier.cpp
+++ plugins/smt_utils/src/functional_verifier.cpp
@@ -67,40 +67,24 @@ namespace hal
                 return ERR("could not verify whether candidate gates implement a less-than comparison: 'nullptr' given as output net");
             }
 
-            u32 size = input_a.size();
+            u32 size = std::max(input_a.size(), input_b.size());
             if (size == 0)
             {
                 return ERR("could not verify whether candidate gates implement a less-than comparison: no inputs provided");
             }
-            else if (size != input_b.size())
+
+            auto var_a = get_concat_net_function(input_a, size);
+            if (var_a.is_error())
             {
-                return ERR("could not verify whether candidate gates implement a less-than comparison: input sizes do not match");
+                return ERR_APPEND(var_a.get_error(), "could not verify whether candidate gates implement a less-than comparison: could not concatenate nets of input A to Boolean function");
+            }
+            auto var_b = get_concat_net_function(input_b, size);
+            if (var_b.is_error())
+            {
+                return ERR_APPEND(var_b.get_error(), "could not verify whether candidate gates implement a less-than comparison: could not concatenate nets of input B to Boolean function");
             }
 
-            // TODO replace with get_concat_net_function
-            auto var_a = BooleanFunction::Var("net_" + std::to_string(input_a.front()->get_id()), 1);
-            auto var_b = BooleanFunction::Var("net_" + std::to_string(input_b.front()->get_id()), 1);
-            for (u32 i = 1; i < size; i++)
-            {
-                if (auto var_a_res = BooleanFunction::Concat(BooleanFunction::Var("net_" + std::to_string(input_a.at(i)->get_id()), 1), var_a.clone(), i + 1); var_a_res.is_error())
-                {
-                    var_a = var_a_res.get();
-                }
-                else
-                {
-                    return ERR_APPEND(var_a_res.get_error(), "could not verify whether candidate gates implement a less-than comparison: unable to concatenate variables of input A");
-                }
-                if (auto var_b_res = BooleanFunction::Concat(BooleanFunction::Var("net_" + std::to_string(input_b.at(i)->get_id()), 1), var_b.clone(), i + 1); var_b_res.is_ok())
-                {
-                    var_b = var_b_res.get();
-                }
-                else
-                {
-                    return ERR_APPEND(var_b_res.get_error(), "could not verify whether candidate gates implement a less-than comparison: unable to concatenate variables of input B");
-                }
-            }
-
-            return BooleanFunction::Ult(std::move(var_a), std::move(var_b), size)
+            return BooleanFunction::Ult(var_a.get(), var_b.get(), size)
                 .map<BooleanFunction>([&](auto&& ult_bf) -> Result<BooleanFunction> {
                     return netlist_utils::get_subgraph_function(output, candidate_gates).map<BooleanFunction>([&](auto&& subgraph_bf) -> Result<BooleanFunction> {
                         return BooleanFunction::Eq(std::move(ult_bf), std::move(subgraph_bf), size);
