a0dc0369c9c64a4c0a090023c201f4e1b72a79ed
joern274 <joern274@gmail.com>
2024-07-01 12:36:56 +0200

    Bugfix: smallset must produce compatible order of u64 when converted to array

--- plugins/hawkeye/src/sbox_database.cpp
+++ plugins/hawkeye/src/sbox_database.cpp
@@ -166,7 +166,8 @@ smallset_t smallset_t::shuffle(u8 shift) const
 
 void smallset_t::to_array(u64* arr) const
 {
-    memcpy(arr, dw64, sizeof(dw64));
+    for (int i=0; i<3; i++)
+        arr[i] = dw64[3-i];
 }
 
 void smallset_t::set(u8 bit)
