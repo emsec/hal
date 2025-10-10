6c6cd8fded58cdc0e249738891608a0b2e15f8ca
Sascha Tommasone <sascha.tommasone@mpi-sp.org>
2025-10-10 08:48:46 -0400

    add resend last message command to helix publisher debug script

--- plugins/helix/scripts/publisher.py
+++ plugins/helix/scripts/publisher.py
@@ -10,18 +10,23 @@ except ImportError:
 
 def main() -> None:
     sequence_number: int = 0
+    last_message: str = ""
     client: redis.Redis = redis.Redis(host="localhost", port=6379, db=0)
 
     while True:
         channels: list[str] = input("Enter channels: ").split()
         if not channels:
             break
+        elif channels[0] == "rs" and sequence_number > 0:
+            client.publish(channel, last_message)
+            continue
 
         command: str = input("Enter command: ")
         identifiers: list[str] = input("Enter identifiers: ").split()
 
         for channel in channels:
-            client.publish(channel, json.dumps({"sequence_number": sequence_number, "command": command,"instances": identifiers}))
+            last_message: str = json.dumps({"sequence_number": sequence_number, "command": command,"instances": identifiers})
+            client.publish(channel, last_message)
 
         sequence_number += 1
         print()
